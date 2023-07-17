925b99c78421569f2d6bf542a685d0200e5621d3
winnie54 <111205676+winnie54@users.noreply.github.com>
2023-07-17 12:17:15 +0100

    modified

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -2,7 +2,7 @@
 word = "Holberton"
 word_first_3 = "hol"
 word_last_2 = "on"
-middle_word = "olberton"
+middle_word = "olberto"
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
 print("Middle word: {}".format(middle_word))
