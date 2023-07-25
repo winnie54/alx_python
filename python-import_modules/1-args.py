485881e53b59d8b5b068233b4fbea2d27b6abec0
winnie54 <111205676+winnie54@users.noreply.github.com>
2023-07-25 18:23:23 +0100

    update

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
+import sys
+num_args = len(sys.argv) - 1
+arg_list = sys.argv[1:]
+if num_args == 0:
+    print("0 arguments.")
+elif num_args == 1:
+    print("1 argument:")
+else:
+    print("{}arguments:".format(num_args))
+for i in range(num_args):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+
+"""if __name__ == "__main__":
     #prints the number of and the list of its arguments.
     import sys
 
-    count = len(sys.argv) - 1
-    if count == 0:
+    num_args = len(sys.argv) - 1
+    if num_args == 0:
         print("0 arguments.")
-    elif count == 1:
+    elif num_args == 1:
         print("1 argument:")
-    else:print("{}arguments:".format(count))
+    else:
+        print("{}arguments:".format(num_args./1-a   ))
 
-    for i in range(count):
+    for i in range(num_args):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
+ """
