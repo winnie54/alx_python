1970ae72edf3363e98115c894a5965ca3ea82fe2
winnie54 <111205676+winnie54@users.noreply.github.com>
2023-08-06 16:35:03 +0100

    python class

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -12,15 +12,16 @@ class Square:
         self.__size = size
     def get_size(self):
         return self.__size
-    """def __init__(self, size):
+    def __init__(self, size):
         #initialize square
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size"""
+        self.__size = size
     
         def __validate_size(self):
+            #validates a square
         if not isinstance(self.__size, int):
             raise TypeError("size must be an integer")
         
